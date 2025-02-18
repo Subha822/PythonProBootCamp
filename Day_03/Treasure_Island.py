26bc82cbef8a6be9f169f8c180a6e4ac4b44d5af
Subha Rajagopal <73748056+Subha822@users.noreply.github.com>
2025-02-18 10:54:42 +0000

    blind_auction

--- Day_03/Treasure_Island.py
+++ Day_03/Treasure_Island.py
@@ -23,4 +23,27 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
 print("You're at a cross road. Where do you want to go ? ")
-input('Type "left" or "right" ')
+choice1 = input('Type "left" or "right" \n')
+if choice1=="left":
+    choice2=input('You\'ve come to a lake. '
+                  'There is an island in the middle of the lake. '
+                  'Type "wait" to wait for a boat. '
+                  'Type "swim" to swim across.').lower()
+    if choice2=="wait":
+        choice3 = input("You arrive at the island unharmerd. "
+                        "There is house with 3 doors. One red, "
+                        "one yellow and one blue. "
+                        "Which colour do you choose?").lower()
+        if choice3 == "red":
+            print("It's a room full of fire. Game Over")
+        elif choice3 == "yellow":
+            print("You found the treasure. You Win!")
+        elif choice3 == "blue":
+            print("You enter a room of beasts. Game Over.")
+        else:
+            print("You chose a door that doesn't exist. Game Over.")
+    else:
+        print("You got attacked by an angry trout. Game Over.")
+else:
+    print("You fell in to a hole. Game Over.")
+
