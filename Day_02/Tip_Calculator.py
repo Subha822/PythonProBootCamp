641a3defb58b2adf89e46d2fc5c96e524e230927
Subha Rajagopal <73748056+Subha822@users.noreply.github.com>
2025-03-20 02:56:21 +0000

    Changes

--- Day_02/Tip_Calculator.py
+++ Day_02/Tip_Calculator.py
@@ -1,9 +1,10 @@
-print("Welcome to the tip calculator!.")
-total_bill=float(input("What was the total bill? \n$"))
-tip = int(input("How much tip would you like to give ? 10, 12, or 15? \n"))
-people_split=int(input("How may people to split the bill? \n"))
-output=(total_bill)/people_split*(1+(tip/100))
-print(f"Each person should pay: ${output:.2f}")
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+people = int(input("How many people to split the bill? "))
+tip = bill * (tip / 100) + bill
+total_pay = tip / people
+print(f'Each person should pay: ${total_pay:.2f}')
 
 
 # Output:
---
+++ Day_18/main.py
@@ -0,0 +1,4 @@
+import turtle
+
+if __name__ == "__main__":
+    print("Turtle graphics won't work in this environment.")
