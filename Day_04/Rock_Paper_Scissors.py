61986781b6629fc212a4bd46fa7f0c631766d7a0
Subha Rajagopal <73748056+Subha822@users.noreply.github.com>
2025-03-21 05:58:14 +0000

    Changes

--- Day_04/Rock_Paper_Scissors.py
+++ Day_04/Rock_Paper_Scissors.py
@@ -44,3 +44,54 @@ elif user_choice > computer_choice:
     print("You win!")
 elif computer_choice == user_choice:
     print("It's a draw!")
+
+
+# -------------------------
+
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+game_choice = [rock,paper,scissors]
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+user_choice_pic = game_choice[user_choice]
+print(user_choice_pic)
+
+print("Computer Chose:")
+computer_choice_number = random.randint(0,2)
+computer_choice = game_choice[computer_choice_number]
+print(computer_choice)
+
+if user_choice == computer_choice_number:
+    print("Your Match Draw")
+elif user_choice == 0 and computer_choice_number == 1 or user_choice == 1 and computer_choice_number == 2 or user_choice == 2 and computer_choice_number == 0:
+    print("You Lose")
+elif user_choice == 0 and computer_choice_number == 2 or user_choice == 1 and computer_choice_number == 0 or user_choice == 2 and computer_choice_number == 1:
+    print("You Win")
+
+
