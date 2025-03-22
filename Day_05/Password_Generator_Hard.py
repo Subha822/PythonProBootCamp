77c577d9c1fb5e188ae0c07a1257d3a6ada7dc1d
Subha Rajagopal <73748056+Subha822@users.noreply.github.com>
2025-03-22 07:22:20 +0000

    Day_05 Update

--- Day_05/Password_Generator_Hard.py
+++ Day_05/Password_Generator_Hard.py
@@ -30,3 +30,25 @@ for char in password_list:
   password += char
 
 print(f"Your password is: {password}")
+
+# -------------------------
+
+
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+choose_letters = random.sample(letters,nr_letters)
+choose_symbols = random.sample(symbols,nr_symbols)
+choose_numbers = random.sample(numbers,nr_numbers)
+array = choose_letters+choose_symbols+choose_numbers
+res = random.sample(array,len(array))
+res = ''.join(res)
+print(res)
