785dbc60cfd6430c6e3b78fdbb43882d0e0278ab
Subha Rajagopal <73748056+Subha822@users.noreply.github.com>
2025-03-24 11:50:51 +0000

    update

--- Day_07/Hangman.py
+++ Day_07/Hangman.py
@@ -1,7 +1,6 @@
 import random
 import hangman_words
 import hangman_art
-
 lives = 6
 chosen_word = random.choice(hangman_words.word_list)
 print(chosen_word)
@@ -13,7 +12,7 @@ print("Word to guess: " + placeholder)
 game_over = False
 correct_letters = []
 while not game_over:
-    print("****************************<???>/6 LIVES LEFT****************************")
+    print(f"**************************** {lives}/6 LIVES LEFT****************************")
     guess = input("Guess a letter: ").lower()
     display = ""
     for letter in chosen_word:
@@ -27,12 +26,9 @@ while not game_over:
     print("Word to guess: " + display)
     if guess not in chosen_word:
         lives -= 1
-
         if lives == 0:
             game_over = True
-
             print(f"***********************YOU LOSE**********************")
-
     if "_" not in display:
         game_over = True
         print("****************************YOU WIN****************************")
